b65138337f92194a75df3a30fa4c7a0de7441b6d
JacekSkarupa <jacek.skarupa@mindsailors.com>
2024-12-09 11:18:49 +0000

    added it8951 init function

--- main.c
+++ main.c
@@ -313,10 +313,8 @@ void IT8951LoadImageEnd()
     SPIWriteCommand(IT8951_TCON_LD_IMG_END);
 }
 
-
-int main(void) {
-    printf("RaspberryPi SPI test\n");
-
+bool IT8951Init(void)
+{
     int result = wiringPiSetup();
     printf("%d\n", result);
     if (result == -1)
@@ -338,11 +336,28 @@ int main(void) {
 
     digitalWrite(RST, HIGH);
 
+    return 1;
+}
+int main(void) {
+    printf("RaspberryPi SPI test\n");
+
+    bool init_result = IT8951Init();
+    if (init_result)
+    {
+        printf("IT8951 initialized\n");
+    }
+    else
+    {
+        printf("IT8951 initialization failed\n");
+        return 1;
+    }
+    
+    IT8951Reset();
+    IT8951SystemRun();
+
     uint8_t imageBuffer[IMAGE_BUFFER_SIZE] = {0};
     memset(imageBuffer, 0x69, IMAGE_BUFFER_SIZE);
 
-
-    // load image info
     IT8951LoadImgInfo loadImgInfo;
     loadImgInfo.EndianType = IT8951_LDIMG_B_ENDIAN;
     loadImgInfo.PixelFormat = IT8951_4BPP;
@@ -353,13 +368,11 @@ int main(void) {
     printf("SourceBufferAddr: 0x%08x\n", (uint32_t*)imageBuffer);
     printf("ImageBuffer Size: %d\n", sizeof(imageBuffer));
 
-    IT8951Reset();
-    IT8951SystemRun();
-
     IT8951LoadImageStart(&loadImgInfo);
     LCDWaitForReady();
     // TODO dodac funkcje do ladowania obrazu do bufora drivera przez spi
     const int chunkSize = 1024;
+    int result = 0;
     for (int i = 0; i < IMAGE_BUFFER_SIZE; i += chunkSize)
     {
         int size = (i + chunkSize > IMAGE_BUFFER_SIZE) ? IMAGE_BUFFER_SIZE - i : chunkSize;
