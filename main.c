d44b767a1e3180d2c26639ef26d5bb06dab3ec8b
JacekSkarupa <jacek.skarupa@mindsailors.com>
2024-12-11 09:08:47 +0000

    fixed config for building code with pipewire API in vscode

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
+#include <pipewire/pipewire.h>
+// #include <spa/param/video/format-utils.h>
+
 #include "it8951.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
     printf("RaspberryPi SPI test\n");
 
+    pw_init(&argc, &argv);
+
+    fprintf(stdout, "Compiled with libpipewire %s\n" 
+                    "Linked with libpipewire %s\n",
+                    pw_get_headers_version(),
+                    pw_get_library_version());
     
  
     return 0;
